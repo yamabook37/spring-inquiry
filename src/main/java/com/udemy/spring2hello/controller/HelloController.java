f1c3e5fa70e56525daa1d77f581c9567ce086e66
yamabook37 <yuki.yamamoto0711@gmail.com>
2024-02-11 01:57:09 +0900

    Update

--- src/main/java/com/udemy/spring2hello/controller/HelloController.java
+++ src/main/java/com/udemy/spring2hello/controller/HelloController.java
@@ -1,9 +1,19 @@
 package com.udemy.spring2hello.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @GetMapping(value = "/")
+    public  String index() {
+        return "Access Successes";
+    }
+
+    @GetMapping(value = "/hello")
+    public  String hello() {
+        return "Hello world";
+    }
 
 }
